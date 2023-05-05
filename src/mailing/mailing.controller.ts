4a0e3cfe86a911ebe322c2b36452dee959cba45b
Callum Buttery <callumbuttery@icloud.com>
2023-05-05 12:21:40 +0100

    Feat: basic working endpoint

--- src/mailing/mailing.controller.ts
+++ src/mailing/mailing.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { MailingService } from './mailing.service';
+import { Controller, Post, Body } from '@nestjs/common';
 
 @Controller('mailing')
-export class MailingController {}
+export class MailingController {
+    constructor(private readonly mailingService: MailingService) {}
+
+    @Post('/add')
+    async addToMailingList(@Body() details) {
+        console.log('hitting controller: ', details)
+        const { email } = details
+        return this.mailingService.addToMailingList(email);
+    }
+}
--- src/mailing/mailing.service.ts
+++ src/mailing/mailing.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class MailingService {}
+export class MailingService {
+    addToMailingList(email: string) {
+        console.log('hitting service: ', email)
+        return email;
+    }
+}
